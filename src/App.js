9818f15bacff55794df315d4443c6c6b164f47e8
Zorgi Gitter <zorgi.gitter@gmail.com>
2021-04-04 23:04:11 -0400

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          My awesome GitHub Page running React
         </p>
         <a
           className="App-link"
